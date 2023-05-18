97d5b0a291add8df329d054f2421928dc81520ae
PanzerBiene <panzerbiene@protonmail.com>
2023-05-18 18:03:38 +1000

    Add comments, Improve Formatting

--- items.js
+++ items.js
@@ -1,23 +1,31 @@
+const fs = require('fs');
+
 // DOM elements
-const itemForm = document.querySelector("#item-form")
-const itemList = document.querySelector(".items")
-const mainInput = document.querySelector("#item-form input")
+const itemForm = document.querySelector("#item-form");
+const itemList = document.querySelector(".items");
+const mainInput = document.querySelector("#item-form input");
 
 let items = JSON.parse(localStorage.getItem("items")) || [];
 
 if (localStorage.getItem("items")) {
-    items.map((item) => {
-        createItem(item)
+
+     items.map((item) => {
+        createItem(item);
     })
 }
 
+/**
+ * Listen for a submit action on the item form
+ * Call createItem with the new item
+ * add the item to the item list and saves it in storage
+ */
 itemForm.addEventListener("submit", (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    const inputValue = mainInput.value 
+    const inputValue = mainInput.value ;
 
     if (inputValue == "") {
-        return 
+        return ;
     }
 
     const item = {
@@ -25,85 +33,68 @@ itemForm.addEventListener("submit", (e) => {
         name: inputValue,
         amount: 0,
         isInStock: true
-    }
+    };
 
-    items.push(item)
-    localStorage.setItem("items", JSON.stringify(items))
+    items.push(item);
+    localStorage.setItem("items", JSON.stringify(items));
 
-    createItem(item)
+    createItem(item);
 
-    itemForm.reset()
-    mainInput.focus()
+    itemForm.reset();
+    mainInput.focus();
 })
 
+/**
+ * Listen for click event on the itemList element
+ * If the remove-item button is clicked call removeItem with the id of the item
+ * If update-item is clicked call updateItem
+ */
 itemList.addEventListener("click", (e) => {
+
     if (e.target.classList.contains("remove-item") || 
     e.target.parentElement.classList.contains("remove-item") || 
     e.target.parentElement.parentElement.classList.contains("remove-item")) {
-        const itemId = e.target.closest("li").id
-        removeItem(itemId)
+
+        const itemId = e.target.closest("li").id;
+        removeItem(itemId);
     }
     else if (e.target.classList.contains("update-item") ||
     e.target.parentElement.classList.contains("update-item") || 
     e.target.parentElement.parentElement.classList.contains("update-item")) {
         
-        let list_items = document.getElementsByClassName("items")[0].getElementsByTagName("li")
-        let new_items = []
-        for (i=0; i< list_items.length; i++) {
-            let id = list_items.item(i).getAttribute("id")
-            let spans = list_items.item(i).getElementsByTagName("div")[0].getElementsByTagName("span");
-            let name = spans[0].textContent;
-            let amount = spans[1].textContent;
-            let isInStock = false;
-            if (amount <= 0) {
-                amount = 0
-                isInStock = true;
-            }
-
-            let item = {
-                id: id,
-                name: name,
-                amount: amount,
-                isInStock: isInStock
-            }
-            console.log(item)
-            new_items.push(item);
-        }
-        localStorage.setItem("items", JSON.stringify(new_items))
-
+        updateItem();
     }
+
 })
 
+/**
+ * Listen for keydown event on the itemList element
+ * if the key was a spacebar prevent newline and remove focus from element
+ */
 itemList.addEventListener("keydown", (e) => {
     if (e.keyCode == 13) {
-        e.preventDefault()
-        console.log(e.target)
-
-        if (e.target.id == "name") {
-            itemName = e.target.innerText
-            itemAmount = e.target.amount
-        }
-        else if (e.target.id == "amount") {
-            itemName = e.target.name
-            itemAmount = e.target.innerText
-        }
-
-        updateItem(e.target.parentElement.parentElement.id, itemName, itemAmount)
-        e.target.blur()
+        e.preventDefault();
+        e.target.blur();
     }
 })
 
+/**
+ * Create a list item for the new item
+ * Create the markup for the item
+ * Append the item as a child element of the itemList element
+ * @param {Object} item 
+ */
 function createItem(item) {
-    const itemEl = document.createElement("li")
+    const itemEl = document.createElement("li");
     
-    itemEl.setAttribute("id", item.id)
+    itemEl.setAttribute("id", item.id);
 
     if((!item.isInStock && item.amount <= 0) || item.amount <=0) {
-        itemEl.classList.add("out-of-stock")
-        item.amount = 0
+        itemEl.classList.add("out-of-stock");
+        item.amount = 0;
     }
     else if (item.amount > 0) {
-        item.isInStock = true
+        item.isInStock = true;
     }
 
     const itemElMarkup = `
@@ -134,17 +125,67 @@ function createItem(item) {
                 </svg>
             </button>
         </div>
-                        `
+                        `;
           
-    itemEl.innerHTML = itemElMarkup
+    itemEl.innerHTML = itemElMarkup;
 
-    itemList.appendChild(itemEl)
+    itemList.appendChild(itemEl);
 }
 
+/**
+ * Remove the item with the corrisponding id from items
+ * Save items to storage
+ * Remove the element with the id from the document
+ * @param {number} itemId 
+ */
 function removeItem(itemId) {
-    items = items.filter((item) => item.id != parseInt(itemId))
+    items = items.filter((item) => item.id != parseInt(itemId));
 
-    localStorage.setItem("items", JSON.stringify(items))
+    localStorage.setItem("items", JSON.stringify(items));
 
-    document.getElementById(itemId).remove()
+    document.getElementById(itemId).remove();
+}
+
+/**
+ * Get the list of item elements
+ * For each of the items get the text values for name and amount
+ * Create new item object for each item with new values
+ * Push new item to new_items list
+ * Save new_items list to storage
+ */
+function updateItem() {
+
+    let list_items = document
+    .getElementsByClassName("items")[0]
+    .getElementsByTagName("li");
+
+    let new_items = [];
+
+    for (i=0; i< list_items.length; i++) {
+        let id = list_items.item(i).getAttribute("id");
+
+        let spans = list_items.item(i)
+        .getElementsByTagName("div")[0]
+        .getElementsByTagName("span");
+
+        let name = spans[0].textContent;
+        let amount = spans[1].textContent;
+        let isInStock = false;
+
+        if (amount <= 0) {
+            amount = 0;
+            isInStock = true;
+        }
+
+        let item = {
+            id: id,
+            name: name,
+            amount: amount,
+            isInStock: isInStock
+        };
+
+        new_items.push(item);
+    }
+
+    localStorage.setItem("items", JSON.stringify(new_items));
 }
