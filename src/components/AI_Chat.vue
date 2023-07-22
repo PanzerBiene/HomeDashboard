8481108f470c8af4a9c62e5e95dde06558dd60a7
PanzerBiene <panzerbiene@protonmail.com>
2023-07-22 15:27:07 +1000

    add base AI_hat component

---
+++ src/components/AI_Chat.vue
@@ -0,0 +1,24 @@
+<script>
+import { Configuration, OpenAIApi } from "openai";
+
+export default{
+    props: {
+    },
+    methods: {
+        send_to_ai() {
+            
+        }
+    }
+};
+
+</script>
+
+<template>
+    <br>
+    <div class="chat_history">
+        
+    </div>
+    <label for="prompt">Your message: </label>
+    <input type="text" id="prompt">
+    <button id="prompt-submit">Send</button>
+</template>
