10037c1fd8d633eb1c14b095e6addab0a692f56b
PanzerBiene <panzerbiene@protonmail.com>
2023-07-23 14:49:21 +1000

    configure to send items/messages to ai endpoint

--- src/views/AI.vue
+++ src/views/AI.vue
@@ -1,14 +1,30 @@
 <script setup>
 import ai_chat from '../components/AI_Chat.vue';
 import { useItemsStore } from '../stores/items.js';
+import axios from 'axios';
 
 const item_store = useItemsStore()
 const items = item_store.get_new_items;
+
+async function send_to_ai(messages) {
+    alert('sent to ai');
+    const url = "https://zjvhedhed9.execute-api.ap-southeast-2.amazonaws.com/prod/chatai";
+    const data = {
+        pantry_items: items,
+        chat_messages: messages,
+    };
+    const config = {
+        'x-api-key': '',
+        'Access-Control-Allow-Origin': '*',
+    };
+
+    const response = await axios.post(url, data, config);
+}
 </script>
 <template>
 
 <main>    
-    <ai_chat :items=items></ai_chat>
+    <ai_chat :items=items @SENDTOAI="send_to_ai"></ai_chat>
 </main>
 
 </template>
