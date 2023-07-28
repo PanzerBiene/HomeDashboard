71840220ec02175708daed5a24c4a9c6987d137b
PanzerBiene <panzerbiene@protonmail.com>
2023-07-28 19:58:17 +1000

    add list of ai messages

--- src/components/AI_Chat.vue
+++ src/components/AI_Chat.vue
@@ -2,11 +2,13 @@
 export default{
     props: {
         items: Array,
+        ai_messages: Array,
     },
     emits: ['SENDTOAI'],
     methods: {
         get_messages() {
-            return ["test"];
+            prompt = document.getElementById("prompt").value;
+            return [{"role": "You", "content": prompt}];
         }
     }
 };
@@ -16,7 +18,12 @@ export default{
 <template>
     <br>
     <div class="chat_history">
-        
+        <ul>
+            <li v-for="(ai_message, index) in ai_messages" :key="index">
+                <span>{{ ai_message.role }}</span>
+                <span>{{ ai_message.content }}</span>
+            </li>
+        </ul>
     </div>
     <label for="prompt">Your message: </label>
     <input type="text" id="prompt">
