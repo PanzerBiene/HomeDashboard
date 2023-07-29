6db3ef61958502bacd412f6c40d75d1a52224f31
PanzerBiene <panzerbiene@protonmail.com>
2023-07-29 11:09:57 +1000

    add shopping list to router

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ import { RouterLink, RouterView } from 'vue-router';
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/items">Items</RouterLink>
+        <RouterLink to="/shopping_list">Shopping List</RouterLink>
         <RouterLink to="/chat">AI Chat</RouterLink>
         <RouterLink to="/preferences">Preferences</RouterLink>
       </nav>
