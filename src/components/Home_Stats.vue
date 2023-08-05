c82dda718e65b735c7c282742dbdb617d27ffd01
PanzerBiene <panzerbiene@protonmail.com>
2023-08-05 15:20:53 +1000

    add bootstrap classes to stats

--- src/components/Home_Stats.vue
+++ src/components/Home_Stats.vue
@@ -11,10 +11,12 @@ export default{
 
 <template>
     <section>
-            <ul class="metrics">
-                <li id="items-in-shopping-list">Items in shopping list: {{ shopping_list.length }}</li>
-                <li id="items-in-db">Items in list: {{ items.length }}</li>
-                <li id="items-in-todo">Items in todo list: {{ todo_list.length }}</li>
-            </ul>
-        </section>
+        <div class="container">
+            <div class="metrics row mt-4">
+                <div class="col col-lg-3.5 border bg-light rounded mx-2" id="items-in-shopping-list">Items in shopping list: {{ shopping_list.length }}</div>
+                <div class="col col-lg-3.5 border bg-light rounded mx-2" id="items-in-db">Items in list: {{ items.length }}</div>
+                <div class="col col-lg-3.5 border bg-light rounded mx-2" id="items-in-todo">Items in todo list: {{ todo_list.length }}</div>
+            </div>
+        </div>
+    </section>
 </template>
